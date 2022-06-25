28f8f2c6e7cc54e123d98700496cf3529aba908e
John T. Wodder II <git@varonathe.org>
2022-06-25 08:47:37 -0400

    Packaging

--- src/gimatch/__init__.py
+++ src/gimatch/__init__.py
@@ -1,11 +1,25 @@
+"""
+Gitignore-style path matching
+
+Visit <https://github.com/jwodder/gimatch> for more information.
+"""
+
 from __future__ import annotations
 from collections.abc import Iterable
 import os
-from typing import Generic, AnyStr
+from typing import AnyStr, Generic
+
+__version__ = "0.1.0.dev1"
+__author__ = "John Thorvald Wodder II"
+__author_email__ = "gimatch@varonathe.org"
+__license__ = "MIT"
+__url__ = "https://github.com/jwodder/gimatch"
+
 
 class Gitignore(Generic[AnyStr]):
     def match(self, path: AnyStr | os.PathLike[AnyStr], is_dir: bool = False) -> bool:
         raise NotImplementedError
+
 
 def compile(patterns: Iterable[AnyStr], ignorecase: bool = False) -> Gitignore[AnyStr]:
     raise NotImplementedError
